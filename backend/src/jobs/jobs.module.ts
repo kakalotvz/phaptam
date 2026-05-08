f5418125afcfc44ffd7ce5a47e483357ec30659e
kakalotvz <leminhkhoa25794@gmail.com>
2026-05-08 11:10:26 +0700

    Import storage module in jobs

--- backend/src/jobs/jobs.module.ts
+++ backend/src/jobs/jobs.module.ts
@@ -1,10 +1,12 @@
 import { BullModule } from '@nestjs/bullmq';
 import { Module } from '@nestjs/common';
+import { StorageModule } from '../storage/storage.module';
 import { ImageProcessor } from './image.processor';
 import { RssProcessor } from './rss.processor';
 
 @Module({
   imports: [
+    StorageModule,
     BullModule.registerQueue({ name: 'rss' }),
     BullModule.registerQueue({ name: 'image' }),
     BullModule.registerQueue({ name: 'cache' }),
